eb98fdcd6e71d8acd2b2156ffc22744b41c80393
MIHIR PATEL <mihirptl89@gmail.com>
2023-12-29 10:08:46 -0600

    update big imposter

--- src/pages/work.js
+++ src/pages/work.js
@@ -25,7 +25,7 @@ const WorkIndex = ({ data, location }) => {
 
       <h2>Big Imposter</h2>
       <div>
-        <StaticImage src="../images/big_imposter.png" alt="big_imposter" className="gallery-image" />
+        <StaticImage src="../images/bigimposter_work.png" alt="big_imposter" className="gallery-image" />
       </div>
       <p><strong>[2023 - Current]</strong></p>
       <p><strong>Story</strong>: A side project on imposter syndrome. This project is helping me explore creativity and learn graphic design skills while exploring ways to beat my own big imposter. Don't let the imposter syndrome bankrupt you from doing. If in doubt, take the first step because doubt kills more dreams than failures.</p>
