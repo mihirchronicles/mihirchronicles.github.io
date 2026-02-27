36113436d537cbdaf58861f34e26bd098c4d730d
MIHIR PATEL <mihirptl89@gmail.com>
2026-02-27 13:16:42 -0600

    update layout

--- src/pages/work.js
+++ src/pages/work.js
@@ -28,8 +28,7 @@ const ImageContainer = styled.div`
   
   .gallery-image {
     margin-bottom: 1rem;
-    border-radius: 8px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
   }
 `
 
