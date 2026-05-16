1359e62597c5a226223add82cbb4baa80786b44e
MIHIR PATEL <mihirptl89@gmail.com>
2026-05-16 07:13:26 -0500

    update bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `The Mihir Chronicles`,
     author: {
       name: `Me here, here!`,
-      summary: `This is my playground 🕹️. A collection of notes 📓, book summaries 📚, artwork 🎨 & work projects 💼. I build products for living. A student of capital markets & human psychology.`,
+      summary: `This is my playground 🕹️. A collection of notes 📓, book summaries 📚, artwork 🎨 & work projects 💼. I build products for living.`,
     },
     description: `A personal site of Mihir Patel. Book summaries, deepdive notes, short essays and art.`,
     siteUrl: `https://mihirchronicles.com/`,
