b6b250f7bd3a8ae8fdc8838b8e79a4fd23c4026a
MIHIR PATEL <mihirptl89@gmail.com>
2026-08-11 07:52:23 -0500

    remove substack

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,9 +41,9 @@ const Layout = ({ location, title, children, isBlogPost = false }) => {
         <hr></hr>
         <p><i>Drop me a note if you are curious to chat. Newsletter goes out once a year in December.</i></p>
         <div style={{ marginBottom: '1rem' }}>
-          <a href="https://mihirchronicles.substack.com/"><strong>Substack</strong></a>
+          {/* <a href="https://mihirchronicles.substack.com/"><strong>Substack</strong></a>
           {` `}
-          <span> | </span>
+          <span> | </span> */}
           <a href="https://www.linkedin.com/in/mihirchronicles/"><strong>Linkedin</strong></a>
           {` `}
           <span> | </span>
